3c556e8766d85c6533aa7b2203209f1bcf17e9e5
Jasper Sandhu <js010582@js010582-mbp.local>
2023-03-25 15:29:04 -0700

    Added additional examples

---
+++ Example_Prog/pyvisa_example.py
@@ -0,0 +1,23 @@
+import pyvisa
+
+# Create a resource manager object
+rm = pyvisa.ResourceManager()
+
+# Find the instrument's VISA address (you may need to change the string)
+instrument_address = 'USB0::0x1234::0x5678::INSTR'
+instrument = rm.open_resource(instrument_address)
+
+# Send a command to the instrument
+instrument.write('MEASure:VOLTage:DC?')
+
+# Read the response from the instrument
+response = instrument.read()
+
+# Print the response
+print('The voltage is {} V'.format(response.strip()))
+
+# Close the instrument
+instrument.close()
+
+# Close the resource manager
+rm.close()
